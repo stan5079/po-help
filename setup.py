4e45be54960b6a41100529781a9ce6622ef992f8
Hendrikus Petrus Mertens <stnmertens@gmail.com>
2022-05-23 14:50:55 +0200

    setup.py minimum python requirement

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license='GNU General Public License v3.0',
     author='Stan Mertens',
     description='A small helper repository for handling .po files.',
+    python_requires='>=3.9',
     install_requires=[
         'googletrans-py==4.0.0'
     ]
